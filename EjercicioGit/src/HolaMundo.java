63e30e05b2f01f3cbb2c81882cf39c02bede03d1
ilarraya <56032245+ilarraya@users.noreply.github.com>
2019-10-01 19:04:20 +0200

    Update HolaMundo.java

--- EjercicioGit/src/HolaMundo.java
+++ EjercicioGit/src/HolaMundo.java
@@ -5,5 +5,6 @@ public class HolaMundo {
 		System.out.println("Hola mundo");
 		System.out.println("Adios");
 		System.out.println("He vuelto");
+		System.out.println("He vuelto yo tambien");
 	}
 }
